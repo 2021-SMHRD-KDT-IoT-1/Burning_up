3466a90264c53a0d49d4c3507f8d8838df6b5e17
KSW <rlatjsdnd325@naver.com>
2021-07-27 16:49:20 +0900

    대략적 화면 연결(start-loginpage-joinpage

--- app/src/main/java/kr/os/burning_up/LoginActivity_M.java
+++ app/src/main/java/kr/os/burning_up/LoginActivity_M.java
@@ -2,13 +2,39 @@ package kr.os.burning_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity_M extends AppCompatActivity {
+    EditText edt_pw_m,edt_id_m;
+    Button btn_login_m, btn_join_m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_m);
+
+        edt_id_m = findViewById(R.id.edt_id_m);
+        edt_pw_m = findViewById(R.id.edt_pw_m);
+        btn_login_m = findViewById(R.id.btn_login_m);
+        btn_join_m = findViewById(R.id.btn_join_m);
+
+        btn_join_m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity_M.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login_m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/kr/os/burning_up/LoginActivity_U.java
+++ app/src/main/java/kr/os/burning_up/LoginActivity_U.java
@@ -2,13 +2,38 @@ package kr.os.burning_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity_U extends AppCompatActivity {
-
+    EditText edt_id_u,edt_pw_u;
+    Button btn_login_u, btn_join_u;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_u);
+
+        edt_id_u = findViewById(R.id.edt_id_u);
+        edt_pw_u = findViewById(R.id.edt_pw_u);
+        btn_join_u = findViewById(R.id.btn_join_u);
+        btn_login_u = findViewById(R.id.btn_login_u);
+
+        btn_join_u.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity_U.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login_u.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
