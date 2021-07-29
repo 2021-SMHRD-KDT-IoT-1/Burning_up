f2b32c95346226ff44a0822cea4f3f8a79f0096d
KSW <rlatjsdnd325@naver.com>
2021-07-29 19:52:42 +0900

    회원가입 (유저) 하는중

--- app/src/main/java/kr/os/burning_up/JoinActivity_U.java
+++ app/src/main/java/kr/os/burning_up/JoinActivity_U.java
@@ -3,12 +3,31 @@ package kr.os.burning_up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class JoinActivity_U extends AppCompatActivity {
+    EditText edt_joinid_u,edt_joinpw_u,edt_name_u,edt_birth_u,edt_tel_u;
+    RadioButton radio_man,radio_woman;
+    RadioGroup gender;
+    Button btn_joinok_u,btn_joincancel_u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_u);
+
+        edt_birth_u = findViewById(R.id.edt_birth_u);
+        edt_tel_u = findViewById(R.id.edt_tel_u);
+        edt_joinid_u = findViewById(R.id.edt_joinid_u);
+        edt_joinpw_u = findViewById(R.id.edt_joinpw_u);
+        edt_name_u = findViewById(R.id.edt_name_u);
+        radio_man = findViewById(R.id.radio_man);
+        radio_woman = findViewById(R.id.radio_woman);
+        gender = findViewById(R.id.gender);
+        btn_joinok_u = findViewById(R.id.btn_joinok_u);
+        btn_joincancel_u = findViewById(R.id.btn_joincancel_u);
     }
 }
