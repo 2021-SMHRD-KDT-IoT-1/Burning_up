580f5f4293976033c1e96b389203b40ff203a8e0
KSW <rlatjsdnd325@naver.com>
2021-07-31 18:11:26 +0900

    관리자 프레그먼트 틀제작

--- app/src/main/java/kr/os/burning_up/MainActivity_M.java
+++ app/src/main/java/kr/os/burning_up/MainActivity_M.java
@@ -10,7 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_M extends AppCompatActivity {
 
-    BottomNavigationView nav1;
+    BottomNavigationView nav1,nav2;
     Mainfrag1 frag1;
     Mainfrag2 frag2;
     Mainfrag3 frag3;
@@ -24,11 +24,14 @@ public class MainActivity_M extends AppCompatActivity {
         setContentView(R.layout.activity_main_m);
 
         nav1 = findViewById(R.id.nav1);
+        nav2 = findViewById(R.id.nav2);
 
         frag1 = new Mainfrag1();
         frag2 = new Mainfrag2();
         frag3 = new Mainfrag3();
         frag4 = new Mainfrag4();
+        frag5 = new Mainfrag5();
+        frag6 = new Mainfrag6();
 
 
         getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag1).commit();
@@ -47,8 +50,20 @@ public class MainActivity_M extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag3).commit();
                 }else if(itemId == R.id.info_m){
                     getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag4).commit();
-                }else{
+                }
+                return true;
+            }
+        });
+        nav2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.myp_m){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag5).commit();
+                }else if(itemId == R.id.alarm){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag6).commit();
                 }
                 return true;
             }
