316f7e2ece2c60f310c0bf747328c57b5cfb3603
KSW <rlatjsdnd325@naver.com>
2021-07-31 17:45:57 +0900

    관리자 프레그먼트 제작중

--- app/src/main/java/kr/os/burning_up/MainActivity_M.java
+++ app/src/main/java/kr/os/burning_up/MainActivity_M.java
@@ -1,68 +1,58 @@
 package kr.os.burning_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_M extends AppCompatActivity {
-    Button btn_FEtag, btn_FEmng, btn_BDmng, btn_FEdisposal, btn_alarm_m, btn_FF_m;
+
+    BottomNavigationView nav1;
+    Mainfrag1 frag1;
+    Mainfrag2 frag2;
+    Mainfrag3 frag3;
+    Mainfrag4 frag4;
+    Mainfrag5 frag5;
+    Mainfrag6 frag6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_m);
-        btn_FEtag = findViewById(R.id.btn_FEtag);
-        btn_FEmng = findViewById(R.id.btn_FEmng);
-        btn_BDmng = findViewById(R.id.btn_BDmng);
-        btn_FEdisposal = findViewById(R.id.btn_FEdisposal);
-        btn_alarm_m = findViewById(R.id.btn_alarm_m);
-        btn_FF_m = findViewById(R.id.btn_FF_m);
 
-        btn_FEtag.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FE_tag_M.class);
-                startActivity(intent);
-            }
-        });
-        btn_FEmng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FE_mng_M.class);
-                startActivity(intent);
-            }
-        });
-        btn_BDmng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BD_mng_M.class);
-                startActivity(intent);
-            }
-        });
-        btn_FEdisposal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FE_disposal_M.class);
-                startActivity(intent);
-            }
-        });
-        btn_alarm_m .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Fire_alarm_M.class);
-                startActivity(intent);
-            }
-        });
-        // 소방정보 작성관리
-        btn_FF_m.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FF_infowrite_M.class);
+        nav1 = findViewById(R.id.nav1);
 
-                startActivity(intent);
+        frag1 = new Mainfrag1();
+        frag2 = new Mainfrag2();
+        frag3 = new Mainfrag3();
+        frag4 = new Mainfrag4();
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag1).commit();
+
+        nav1.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.home_m){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag1).commit();
+                }else if(itemId == R.id.fe_m){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag2).commit();
+                }else if(itemId == R.id.bd_m){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag3).commit();
+                }else if(itemId == R.id.info_m){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag4).commit();
+                }else{
+
+                }
+                return true;
             }
         });
+
     }
 }
