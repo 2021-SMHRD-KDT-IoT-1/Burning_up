98838b3f317faaadcf4264d3235cf366d466510f
KSW <rlatjsdnd325@naver.com>
2021-08-01 16:07:00 +0900

    홈 제작

--- app/src/main/java/kr/os/burning_up/MainActivity_M.java
+++ app/src/main/java/kr/os/burning_up/MainActivity_M.java
@@ -10,13 +10,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_M extends AppCompatActivity {
 
-    BottomNavigationView nav1,nav2;
+    BottomNavigationView nav1;
     Mainfrag1 frag1;
     Mainfrag2 frag2;
     Mainfrag3 frag3;
     Mainfrag4 frag4;
-    Mainfrag5 frag5;
-    Mainfrag6 frag6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +22,13 @@ public class MainActivity_M extends AppCompatActivity {
         setContentView(R.layout.activity_main_m);
 
         nav1 = findViewById(R.id.nav1);
-        nav2 = findViewById(R.id.nav2);
+
 
         frag1 = new Mainfrag1();
         frag2 = new Mainfrag2();
         frag3 = new Mainfrag3();
         frag4 = new Mainfrag4();
-        frag5 = new Mainfrag5();
-        frag6 = new Mainfrag6();
+
 
 
         getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag1).commit();
@@ -54,20 +51,7 @@ public class MainActivity_M extends AppCompatActivity {
                 return true;
             }
         });
-        nav2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                int itemId = item.getItemId();
-
-                if(itemId == R.id.myp_m){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag5).commit();
-                }else if(itemId == R.id.alarm){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame, frag6).commit();
-                }
-                return true;
-            }
-        });
 
     }
 }
