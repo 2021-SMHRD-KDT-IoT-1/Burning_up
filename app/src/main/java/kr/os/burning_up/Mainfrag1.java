1d7432c77157391980e9b1b3242e11acbfdf2e8b
KSW <rlatjsdnd325@naver.com>
2021-08-01 17:35:48 +0900

    스피너 제작

--- app/src/main/java/kr/os/burning_up/Mainfrag1.java
+++ app/src/main/java/kr/os/burning_up/Mainfrag1.java
@@ -7,16 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 
 public class Mainfrag1 extends Fragment {
-
-
+    Spinner spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mainfrag1, container, false);
+        String[] items = {"1층","2층","3층"};
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+
+
 
 
         return view;
