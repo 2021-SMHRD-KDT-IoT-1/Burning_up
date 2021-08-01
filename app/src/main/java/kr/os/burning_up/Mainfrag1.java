0ed402f77ead309e8b37f8e06f8d5823bf0a2c0b
KSW <rlatjsdnd325@naver.com>
2021-08-01 17:58:51 +0900

    스피너 제작완료^^

--- app/src/main/java/kr/os/burning_up/Mainfrag1.java
+++ app/src/main/java/kr/os/burning_up/Mainfrag1.java
@@ -7,19 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 
 public class Mainfrag1 extends Fragment {
     Spinner spinner;
+    String[] items = {"1층","2층","3층"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mainfrag1, container, false);
-        String[] items = {"1층","2층","3층"};
+
 
         spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this.getActivity(), android.R.layout.simple_spinner_item);
+        spinner.setPrompt("층 선택");
+        spinner.setAdapter(adapter);
+        adapter.addAll(items);
 
 
 
