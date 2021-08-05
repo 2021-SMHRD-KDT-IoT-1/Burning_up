58164620ec7a8d04dfc9dd96511b13b42fa1d122
KSW <rlatjsdnd325@naver.com>
2021-08-05 15:41:26 +0900

    건물관리 추가

---
+++ app/src/main/java/kr/os/burning_up/Floar_3.java
@@ -0,0 +1,14 @@
+package kr.os.burning_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Floar_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floar3);
+    }
+}
--- app/src/main/java/kr/os/burning_up/Mainfrag3.java
+++ app/src/main/java/kr/os/burning_up/Mainfrag3.java
@@ -1,5 +1,6 @@
 package kr.os.burning_up;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Mainfrag3 extends Fragment {
-
-
+    Button btn_change, btn_delete;
+    TextView tv_floar3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mainfrag3, container, false);
+
+        btn_change = view.findViewById(R.id.btn_change);
+        btn_delete = view.findViewById(R.id.btn_delete);
+        tv_floar3 = view.findViewById(R.id.tv_floar3);
+
+        tv_floar3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), Floar_3.class);
+                startActivity(intent);
+            }
+        });
 
 
         return view;
