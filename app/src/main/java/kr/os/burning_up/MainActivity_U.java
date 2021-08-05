594e1f324935688a841e43c1adf789ba3d6a6482
KSW <rlatjsdnd325@naver.com>
2021-08-05 17:45:35 +0900

    사용자 프레그먼트 추가

--- app/src/main/java/kr/os/burning_up/MainActivity_U.java
+++ app/src/main/java/kr/os/burning_up/MainActivity_U.java
@@ -1,14 +1,49 @@
 package kr.os.burning_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_U extends AppCompatActivity {
+
+    BottomNavigationView nav2;
+    Mainfrag_U1 frag1;
+    Mainfrag_U2 frag2;
+    Mainfrag_U3 frag3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_u);
+
+        nav2 = findViewById(R.id.nav2);
+
+        frag1 = new Mainfrag_U1();
+        frag2 = new Mainfrag_U2();
+        frag3 = new Mainfrag_U3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.Frame2, frag1).commit();
+
+        nav2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.fe_alarm){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame2, frag1).commit();
+                }else if(itemId == R.id.bd_pic){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame2, frag2).commit();
+                }else if(itemId == R.id.info){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.Frame2, frag3).commit();
+                }
+                return true;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/kr/os/burning_up/Mainfrag_U1.java
@@ -0,0 +1,23 @@
+package kr.os.burning_up;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Mainfrag_U1 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.mainfrag_u1, container, false);
+
+
+        return view;
+    }
+}
