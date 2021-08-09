3aa379f404c13694ff24f57ba92f5a5766caf833
KSW <rlatjsdnd325@naver.com>
2021-08-10 00:52:46 +0900

    사용자 소화기 관리 추가

--- app/src/main/java/kr/os/burning_up/Mainfrag_U1.java
+++ app/src/main/java/kr/os/burning_up/Mainfrag_U1.java
@@ -5,20 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
 
 public class Mainfrag_U1 extends Fragment {
 
+    Spinner spinner3, spinner4;
     Button btn_findfe;
+    String[] item1 = {"1층","2층","3층"};
+    String[] item2 = {"스마트인재개발원","스마트인재캠퍼스"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mainfrag_u1, container, false);
         btn_findfe = view.findViewById(R.id.btn_findfe);
+        spinner3 = (Spinner)view.findViewById(R.id.spinner3);
+        spinner4 = (Spinner)view.findViewById(R.id.spinner4);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_spinner_item);
+        spinner3.setPrompt("층 선택");
+        spinner3.setAdapter(adapter1);
+        adapter1.addAll(item1);
+        spinner3.setPrompt("건물 선택");
+        spinner4.setAdapter(adapter2);
+        adapter2.addAll(item2);
 
         btn_findfe.setOnClickListener(new View.OnClickListener() {
             @Override
